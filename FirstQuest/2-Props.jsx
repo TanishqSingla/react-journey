09f679e2b68bd44b9be00a759970f4a167ad9fbe
TanishqSingla <tanishqsingla00@gmail.com>
2020-04-09 00:01:47 +0530

    The saga continues-1

--- FirstQuest/2-Props.jsx
+++ FirstQuest/2-Props.jsx
@@ -11,3 +11,5 @@ ReactDOM.render(<App />, document.getElementById("root"));
 //3. Use props to render the default Beyonce contact card
 //so the Card component can be reused for other contacts.
 //4. Import the contacts.js file to create card components.
+
+//?The zip file contains the final product
--- FirstQuest/3-Props.jsx
+++ FirstQuest/3-Props.jsx
@@ -1,1 +1,5 @@
-import React from "react";
+//Make a component Avatar for the images of the contacts
+
+//Also make a component which contains the contact info of the cards, and name it as Details
+
+//?The zip file contains the final product
