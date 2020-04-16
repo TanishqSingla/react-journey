facdc46fd14e4ea8528438838e2cd55b5f72b7cd
TanishqSingla <tanishqsingla00@gmail.com>
2020-04-16 19:51:31 +0530

    Leveling up

--- The Lair/Hidden Cave/React-forms.js
+++ The Lair/Hidden Cave/React-forms.js
@@ -1,6 +1,7 @@
 //Gaining the knowledge of React forms
 
 import React from "react";
+import React from "react";
 
 function App() {
   const [name, setName] = React.useState("");
@@ -17,10 +18,45 @@ function App() {
   return (
     <div className="container">
       <h1>Hello {headingText}</h1>
-      <input type="text" placeholder="What's your name" value={name} />
+      <input
+        onChange={handleChange}
+        type="text"
+        placeholder="What's your name"
+        value={name}
+      />
       <button onClick={handleSubmit}>Submit</button>
     </div>
   );
 }
 
-export default App;
+//The below component is just a redaclaration with form tag
+
+function App1() {
+  const [name, setName] = React.useState("");
+  const [headingText, setHeading] = React.useState("");
+
+  function handleChange() {
+    setName(event.target.value);
+  }
+
+  function handleSubmit(event) {
+    setHeading(name);
+
+    event.preventDefault();
+  }
+
+  return (
+    <div className="container">
+      <h1>Hello {headingText}</h1>
+      <form onSubmit={handleSubmit}>
+        <input
+          onChange={handleChange}
+          type="text"
+          placeholder="What's your name"
+          value={name}
+        />
+        <button type="submit"></button>
+      </form>
+    </div>
+  );
+}
