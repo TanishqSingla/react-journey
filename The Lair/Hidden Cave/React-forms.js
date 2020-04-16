1d16c1eaefc4c94bd41c1a01a20ca8fed72269db
TanishqSingla <tanishqsingla00@gmail.com>
2020-04-16 19:43:25 +0530

    unleashed react forms

--- The Lair/Hidden Cave/React-forms.js
+++ The Lair/Hidden Cave/React-forms.js
@@ -1,1 +1,26 @@
 //Gaining the knowledge of React forms
+
+import React from "react";
+
+function App() {
+  const [name, setName] = React.useState("");
+  const [headingText, setHeading] = React.useState("");
+
+  function handleChange(event) {
+    setName(event.target.value);
+  }
+
+  function handleSubmit() {
+    setHeading(name);
+  }
+
+  return (
+    <div className="container">
+      <h1>Hello {headingText}</h1>
+      <input type="text" placeholder="What's your name" value={name} />
+      <button onClick={handleSubmit}>Submit</button>
+    </div>
+  );
+}
+
+export default App;
